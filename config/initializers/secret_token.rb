e556c3094e644f0811af8225516259ce94aa76bc
Chunyang Guo <guochy2012@gmail.com>
2014-01-13 11:10:20 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlipayTest::Application.config.secret_key_base = 'f4a83a64fcea1045ddef478018184b53e2c4e1e9c1d091ebe6ee216daa0d105e18a142b80a542b8c247c5a19467ffa7866ec5f871484979921a0c5eb8f119da7'
